df70c1c4810cd423004a983eeeb729777cd2de8f
saifulkhan <saiful.etc@gmail.com>
2021-09-15 12:27:49 +0100

    create react fc

---
+++ display-ui/src/components/plots/Spectrogram.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+import * as d3 from "d3";
+
+interface SpectrogramProps {
+    key: any
+}
+ 
+const Spectrogram: FC<SpectrogramProps> = (props) => {
+    return ( <> </> );
+}
+ 
+export default Spectrogram;
