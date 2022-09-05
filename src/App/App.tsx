231c1558316bf39f73cb1f9da1a7adfd6f678f41
Karini Govender <karini@vivosa.co.za>
2022-09-05 09:13:00 +0200

    NAL-155: Linting

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,9 @@
 import React, { StrictMode } from 'react';
-import { CacheProvider } from '@emotion/react';
-import { Helmet } from 'react-helmet';
+
 import { Box, CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-//import { createEmotionCache } from '../utils/create-emotion-cache';
+// import { createEmotionCache } from '../utils/create-emotion-cache';
 import { theme } from '../theme';
 // Import all the css files created for d3 charts
 import '../libs/css/spectrogram-plot-table.css';
@@ -15,7 +14,7 @@ import SpectrumPlot from '../components/spectrumPlot/spectrumPlot';
 import Statistics from '../components/statistics/statistics';
 
 // Client-side cache, shared for the whole session of the user in the browser.
-//const clientSideEmotionCache = createEmotionCache();
+// const clientSideEmotionCache = createEmotionCache();
 
 function App() {
   const DashboardLayoutRoot = styled('div')(() => ({
@@ -25,30 +24,30 @@ function App() {
   }));
 
   return (
-      <StrictMode>
-        <StyledEngineProvider injectFirst>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <DashboardLayoutRoot>
-              <Box
-                sx={{
+    <StrictMode>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <DashboardLayoutRoot>
+            <Box
+              sx={{
                   display: 'flex',
                   flex: '1 1 auto',
                   flexDirection: 'column',
                   width: '100%'
                 }}
-              >
-                <Statistics />
-                <SpectrumPlot />
-                <Spectrogram />
-                {/*      Suppressed for now.
+            >
+              <Statistics />
+              <SpectrumPlot />
+              <Spectrogram />
+              {/*      Suppressed for now.
         <Rfi />
         */}
-              </Box>
-            </DashboardLayoutRoot>
-          </ThemeProvider>
-        </StyledEngineProvider>
-      </StrictMode>
+            </Box>
+          </DashboardLayoutRoot>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </StrictMode>
   );
 }
 
