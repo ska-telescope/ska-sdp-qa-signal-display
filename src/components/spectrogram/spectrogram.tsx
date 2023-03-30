3816f725f39a3e484ec3e8f97d8356206bdc3a41
Karini Govender <karini@vivosa.co.za>
2023-03-30 15:01:18 +0200

    NAL-433: Removed useCallback - Not needed

--- src/components/spectrogram/spectrogram.tsx
+++ src/components/spectrogram/spectrogram.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-unresolved */
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Card,
   CardContent,
