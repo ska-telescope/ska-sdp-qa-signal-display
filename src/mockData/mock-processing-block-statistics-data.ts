dc637dc31bd38114258889eb9460bd3363581ace
Dominic Schaff <dominic@schaff.dev>
2023-04-25 08:23:21 +0200

    NAL-500 Updated stats

--- src/mockData/mock-processing-block-statistics-data.ts
+++ src/mockData/mock-processing-block-statistics-data.ts
@@ -6,13 +6,15 @@ const mockProcessingBlockStatisticsData = {
     },
     "statistics": {
         "ingestion_rate": 0,
-        "packet_count": 115,
-        "payloads_received": 2
+        "payloads_received": 2,
+        "receive_active": true
     },
     "processing_block": {
         "state": "new",
         "scan_id": "34",
         "processing_block_id": "test-12345678-1234",
+        "execution_block_id": "eb-20230421-1234",
+        "subarray": "subarray_01",
         "time_since_last_payload": 1
     }
 };
