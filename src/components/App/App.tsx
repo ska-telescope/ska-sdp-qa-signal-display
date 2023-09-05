7474d2cbf3f98c569fc7c1a2c15e667d5bd9b059
Andre Odendaal <andre@vivosa.co.za>
2023-09-05 15:13:17 +0200

    NAL-716: Remove of temp code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -21,7 +21,6 @@ function App() {
 
   return (
     <ThemeProvider theme={theme(themeMode.mode)}>
-      <script src='/env.js' />
       <CssBaseline enableColorScheme />
       <React.Suspense fallback="...is loading">
         {
