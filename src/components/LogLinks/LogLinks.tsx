a4e4c2a39d5be2ca42fd91830b771858567f8fc4
Christian Christelis <cchristelis@ska.ac.za>
2023-09-21 11:46:25 +0200

    NAL-744 adding links to kibana and grafana. Expecting the API to provide these at /log-urls//

---
+++ src/components/LogLinks/LogLinks.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { Box, Typography } from '@mui/material';
+import { DATA_API_URL } from '../../utils/constants';
+
+
+interface LogLinksProps {
+    subArray: string;
+}
+  
+const LogLinks = ({ subArray }: LogLinksProps) => {
+  const [kibanaLink, setKibanaLink] = React.useState('');
+  const [grafanaLink, setGrafanaLink] = React.useState('');
+
+  const openLink = (link) => {
+    if (link === ''){
+      alert('Logs Unavailable');
+    } else {
+      window.open(link, "_blank");
+    }
+  }
+
+  React.useEffect( () => {
+    const fetchData = async () => {
+      await fetch(`${DATA_API_URL}/log-urls/${subArray}`)
+      .then(response => response.json())
+      .then(data => {
+        setKibanaLink(data.kibana);
+        setGrafanaLink(data.grafana);
+      })
+    .catch(() => null);
+    }
+    if (subArray){
+      fetchData().catch(() => null);
+    }
+
+  },[subArray])
+
+  return (
+    <Box>
+      <Typography color="inherit">
+        <td onClick={()=> openLink(grafanaLink)}><u>Grafana Dashboard</u></td>
+        <br />
+        <td onClick={()=> openLink(kibanaLink)}><u>Kibana Logs</u></td>
+      </Typography>
+    </Box>
+  );
+};
+export default LogLinks;
