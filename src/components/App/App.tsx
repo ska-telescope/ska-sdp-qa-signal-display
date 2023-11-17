53326401e9a09145fe347f9046b034b7c8772b47
Jacob Maresca <jacob.maresca@akkodis.com>
2023-11-17 13:24:25 +0000

    NAL-849 added state to parent component and pass function to child

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { CssBaseline, Paper, ThemeProvider } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { Footer, Header, Spacer, SPACER_VERTICAL } from '@ska-telescope/ska-gui-components';
+import { Header, Spacer, SPACER_VERTICAL } from '@ska-telescope/ska-gui-components';
 import { storageObject } from '@ska-telescope/ska-gui-local-storage';
 import Container from '../Container/Container';
 import Loader from '../Loader/Loader';
 import theme from '../../services/theme/theme';
+import SKAOFooter from '../Footer/Footer';
 
 const HEADER_HEIGHT = 70;
 const FOOTER_HEIGHT = 30;
@@ -19,6 +20,12 @@ function App() {
   const toolTip = { skao, mode };
   const version = process.env.VERSION;
 
+  const [data, setData] = React.useState(null)
+
+  const childToParent = (childData) => {
+    setData(childData);
+  }
+
   return (
     <ThemeProvider theme={theme(themeMode.mode)}>
       <CssBaseline enableColorScheme />
@@ -30,10 +37,10 @@ function App() {
         />
         <Paper>
           <Spacer size={HEADER_HEIGHT} axis={SPACER_VERTICAL} />
-          <Container data-testid="containerId" />
+          <Container data-testid="containerId" childToParent={childToParent}/>
           <Spacer size={FOOTER_HEIGHT} axis={SPACER_VERTICAL} />
         </Paper>
-        <Footer testId="footerId" version={version} />
+        <SKAOFooter data-testid="footerId" version={version} config={data}/>
       </React.Suspense>
     </ThemeProvider>
   );
