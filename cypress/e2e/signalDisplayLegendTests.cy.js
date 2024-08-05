bc2cf9040e46079fb677a6e6760feb0988d3de6d
Dominic Schaff <dominic@schaff.dev>
2024-08-05 11:22:37 +0200

    NAL-1100 Uncomment tests

--- cypress/e2e/signalDisplayLegendTests.cy.js
+++ cypress/e2e/signalDisplayLegendTests.cy.js
@@ -5,11 +5,12 @@ context('Signal display', () => {
     })
 
     it('Verify expected legend is usable', () => {
+        cy.wait(5000);
         cy.findAllByTestId("sectionHeader").contains("Legend").should("be.visible");
-        // cy.findAllByTestId("VisibilityOffIcon").eq(3).click({ multiple: true, force: true }); // counts from 0
-        // cy.findAllByTestId("legendGroupingId").click({ multiple: true });
-        // cy.findAllByTestId("noChartData1Card").should("be.visible");
-        // cy.findAllByTestId("noChartData2Card").should("be.visible");
-        // cy.findAllByTestId("legendKey").should("be.visible");
+        cy.findAllByTestId("VisibilityOffIcon").eq(3).click({ multiple: true, force: true }); // counts from 0
+        cy.findAllByTestId("legendGroupingId").click({ multiple: true });
+        cy.findAllByTestId("noChartData1Card").should("be.visible");
+        cy.findAllByTestId("noChartData2Card").should("be.visible");
+        cy.findAllByTestId("legendKey").should("be.visible");
     })
 })
