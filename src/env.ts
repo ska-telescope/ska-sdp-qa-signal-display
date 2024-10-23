47757dbbc6cf7635894bbbe071e15412cea40be6
Andre Odendaal <andre@vivosa.co.za>
2024-10-23 16:22:07 +0200

    NAL-1185: Removed unused Permissions API code

--- src/env.ts
+++ src/env.ts
@@ -21,8 +21,6 @@ type EnvType = {
   REACT_APP_MSENTRA_CLIENT_ID: string;
   REACT_APP_MSENTRA_TENANT_ID: string;
   REACT_APP_MSENTRA_REDIRECT_URI: string;
-  REACT_APP_ALLOW_MOCK_USER_PERMISSIONS: boolean;
-  REACT_APP_PERMISSIONS_API_URI: string;
   REACT_APP_USE_LOCAL_DATA: boolean;
 };
 export const env: EnvType = {
