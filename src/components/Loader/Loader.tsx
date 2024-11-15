926c0153e6280b9d0b23b626195578761a52c403
Andre Odendaal <andre@vivosa.co.za>
2024-11-15 12:44:17 +0200

    NAL-1304: Fixed import of BackDrop

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Box } from '@mui/material';
-import { Progress } from '@ska-telescope/ska-gui-components';
+import { BackDrop, Progress } from '@ska-telescope/ska-gui-components';
 
 export default function Loader() {
   return (
     <Box>
-      <Progress size={100} testId="progressTestId" />
+      <BackDrop open>
+        <Progress size={100} testId="progressTestId" />
+      </BackDrop>
     </Box>
   );
 }
