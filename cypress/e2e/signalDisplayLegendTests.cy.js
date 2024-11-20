609d3a5c6810fbfc6409d738bdb638b5fdc7b15b
Jacob Maresca <jacob.maresca@gmail.com>
2024-11-20 10:04:39 +0000

    NAL-1269 tests

--- cypress/e2e/signalDisplayLegendTests.cy.js
+++ cypress/e2e/signalDisplayLegendTests.cy.js
@@ -7,7 +7,13 @@ context('Signal display', () => {
     it('Verify expected legend is usable', () => {
         cy.wait(5000);
         cy.findAllByTestId("sectionHeader").contains("Legend").should("be.visible");
-        cy.findAllByTestId("VisibilityOffIcon").eq(4).click({ multiple: true, force: true }); // counts from 0
+        cy.findAllByTestId("signalCardId").each(($ele) => {
+            cy.wrap($ele).then(($card) => {
+                if($card.find("[data-testid='VisibilityOffIcon']").length){
+                    cy.wrap($card).find("[data-testid='VisibilityOffIcon']").click();
+                }
+            });
+        });
         cy.findAllByTestId("legendGroupingId").click({ multiple: true });
         cy.findAllByTestId("noChartData1Card").should("be.visible");
         cy.findAllByTestId("noChartData2Card").should("be.visible");
