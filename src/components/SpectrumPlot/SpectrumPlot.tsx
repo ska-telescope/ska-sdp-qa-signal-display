32774df3fc082dc84dcb71ad8abfb38a41dfc0e8
Jacob Maresca <jacob.maresca@gmail.com>
2024-11-20 14:19:50 +0000

    NAL-1269 tests

--- src/components/SpectrumPlot/SpectrumPlot.tsx
+++ src/components/SpectrumPlot/SpectrumPlot.tsx
@@ -23,7 +23,6 @@ import SKAOModal from '../Modal/Modal';
 import WaterfallPlot from '../WaterfallPlot/WaterfallPlot';
 
 interface SpectrumPlotProps {
-  key: string;
   data: object;
   displaySettings: typeof QASettings;
   polarization: string;
