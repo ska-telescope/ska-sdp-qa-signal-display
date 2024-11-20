4f6473346d80d066d30cb01f618b0f5b2d287539
Jacob Maresca <jacob.maresca@gmail.com>
2024-11-20 11:53:51 +0000

    NAL-1269 tests

--- cypress/e2e/signalDisplayTests.cy.js
+++ cypress/e2e/signalDisplayTests.cy.js
@@ -165,6 +165,7 @@ context('Signal display', () => {
     })
 
     it("Verify modals operate as expected", () => {
-        cy.findAllByTestId('test-info-modal').should("be.visible").click();
+        cy.findAllByTestId('test-info-modal').should("be.visible").click({ multiple: true , force: true });
+        cy.get('body').click(0,0);
     })
 })
