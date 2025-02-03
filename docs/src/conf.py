bc1f5e7b687e2c7a5e394d9f078fb6a8c4115797
Dominic Schaff <dominic@schaff.dev>
2025-02-03 15:14:43 +0200

    NAL-1332 New Release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -5,9 +5,9 @@ copyright = "2023, SKAO"
 author = "SKAO, Naledi"
 
 # The short X.Y version
-version = "0.19.0"
+version = "1.0.0"
 # The full version, including alpha/beta/rc tags
-release = "0.19.0"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 
